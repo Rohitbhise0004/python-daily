7621bea82194cd5c1b2a4ffe2b0639b133601828
Rohitbhise0004 <rohitbhise004@gmail.com>
2026-06-19 01:41:12 +0530

    Day 2: Python Data Types Practice

---
+++ day2 datatype.py
@@ -0,0 +1,89 @@
+# Data types in Python
+#Basic data types in Python include:               Additional data types in Python include:
+# String                                            List
+#integer                                            Tuple
+#float                                              dictionary
+#boolean                                            Set
+                                                    
+#String
+
+username =("Rohit")
+print(type(username))
+
+place=("napgur")
+print(type(place))
+
+#Integer
+id = 101
+print(type(id))
+
+password = 102030
+print(type(password))
+
+#Float
+height =5.9
+print(type(height))
+
+salary = 15000.60
+print(type(salary))
+
+#Boolean
+
+ismarried = True
+print(type(ismarried))
+
+ismarried = False
+print(type(ismarried))
+
+#additional data types 
+#list
+numbers =[1,2,3,4,5,6,7,8,9,10]
+print(numbers)
+print(type(numbers))
+
+#tuple
+user =(2,'rohit',5.9,True)
+print(user)
+print(type(user))
+
+#set
+names = {'rohit','sanket','shivam',50.50,True}
+print(names)
+print(type(names))
+
+#dictionary
+emp ={id:101,"name":"rohit","address":"nagpur"}
+print(emp)
+print(type(emp ))
+
+
+#Question 1: Identify Data Types
+
+name ="rohit"
+age=23
+height =5.8
+isstudent = True
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(isstudent))
+
+#question 2: Store Different Data Types
+language = "python"
+version = 3.9
+iseasy = True
+print(type(language))
+print(type(version))
+print(type(iseasy))
+
+
+#Question 3: Find the Data Type
+100
+2.2
+True
+"rohit"
+print(type(100))
+print(type(2.2))
+print(type(True))
+print(type("rohit"))
+
