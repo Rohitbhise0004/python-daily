4017d54ced1e3d2cdcb67f9c4720a4f720230e76
Rohitbhise0004 <rohitbhise004@gmail.com>
2026-06-23 20:23:35 +0530

    Day 7 - Dictionary Practice

---
+++ Day7Dictinoary.py
@@ -0,0 +1,16 @@
+#Dictionary
+
+info = {
+    "key": "value",
+    "name": "rohit",
+    "age": 35,
+    "learning": "coding",
+    "marks" : 94.5,
+    "is adult": True,
+    "subject" : ["math","science","python"]
+}
+print(info)
+
+
+dict ={"name":"rohit","marks":95,"age":15,"cgpa": 98.5,}
+print(dict)
